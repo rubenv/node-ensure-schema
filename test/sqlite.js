d1586d9d5a210f21459aef888ee7c9659f4420af
Ruben Vermeersch <ruben@rocketeer.be>
2014-03-15 13:59:40 +0100

    Create missing fields.

--- test/sqlite.js
+++ test/sqlite.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var assert = require('assert');
 var sqlite3 = require('sqlite3').verbose();
 var ensureSchema = require('..');
@@ -68,5 +69,54 @@ describe('SQLite', function () {
         });
     });
 
-    it('Can add columns');
+    it('Can add columns', function (done) {
+        async.series([
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'INTEGER', { primary: true });
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.id, {
+                        type: 'INTEGER',
+                        options: { primary: true }
+                    });
+                    assert.equal(schema.tables.people.name, undefined);
+
+                    cb(); 
+                });
+            },
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'INTEGER', { primary: true });
+                        this.field('name', 'TEXT');
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.id, {
+                        type: 'INTEGER',
+                        options: { primary: true }
+                    });
+                    assert.deepEqual(schema.tables.people.name, {
+                        type: 'TEXT',
+                        options: { primary: false }
+                    });
+
+                    cb(); 
+                });
+            }
+        ], done);
+    });
 });
