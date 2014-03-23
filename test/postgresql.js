37b17b2ef31272aa64a3b50adac7fb0905531f42
Ruben Vermeersch <ruben@rocketeer.be>
2014-03-23 13:29:53 +0100

    PostgreSQL now up-to-par with SQLite.

--- test/postgresql.js
+++ test/postgresql.js
@@ -27,7 +27,25 @@ describe('PostgreSQL', function () {
             origQuery.apply(this, arguments);
         };
         */
-        db.connect(done);
+        db.connect(function (err) {
+            if (err) {
+                return done(err);
+            }
+
+            postgresMapper.extractSchema(db, function (err, schema) {
+                if (err) {
+                    return done(err);
+                }
+
+                var tables = Object.keys(schema.tables);
+
+                function dropTable(table, cb) {
+                    db.query("DROP TABLE " + table, cb);
+                }
+
+                async.each(tables, dropTable, done);
+            });
+        });
     });
 
     it('Creates tables', function (done) {
@@ -49,5 +67,151 @@ describe('PostgreSQL', function () {
 
             done(); 
         });
+    });
+
+    it('Creates tables with multiple columns and index', function (done) {
+        var schema = function () {
+            this.table("people", function () {
+                this.field('id', 'integer', { primary: true });
+                this.field('name', 'text');
+
+                this.index('nameidx', ['name']);
+                this.index('nameidxunique', ['name'], true);
+            });
+        };
+
+        testSchema(db, schema, function (err, schema) {
+            if (err) {
+                return done(err);
+            }
+
+            assert.deepEqual(schema.tables.people.fields.id, {
+                type: 'integer',
+                options: { primary: true }
+            });
+            assert.deepEqual(schema.tables.people.fields.name, {
+                type: 'text',
+                options: { primary: false }
+            });
+
+            assert.deepEqual(schema.tables.people.indexes.nameidx, {
+                unique: false,
+                fields: ['name']
+            });
+
+            assert.deepEqual(schema.tables.people.indexes.nameidxunique, {
+                unique: true,
+                fields: ['name']
+            });
+
+            done(); 
+        });
+    });
+
+    it('Can add columns', function (done) {
+        async.series([
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'integer', { primary: true });
+                        this.field('value', 'integer', { default: 3 });
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.fields.id, {
+                        type: 'integer',
+                        options: { primary: true }
+                    });
+                    assert.deepEqual(schema.tables.people.fields.value, {
+                        type: 'integer',
+                        options: { primary: false, default: 3 }
+                    });
+                    assert.equal(schema.tables.people.fields.name, undefined);
+
+                    cb(); 
+                });
+            },
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'integer', { primary: true });
+                        this.field('name', 'text');
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.fields.id, {
+                        type: 'integer',
+                        options: { primary: true }
+                    });
+                    assert.deepEqual(schema.tables.people.fields.name, {
+                        type: 'text',
+                        options: { primary: false }
+                    });
+
+                    cb(); 
+                });
+            }
+        ], done);
+    });
+
+    it('Can modify indexes', function (done) {
+        async.series([
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'integer', { primary: true });
+                        this.field('name', 'text');
+                            
+                        this.index('nameidx', ['name']);
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.indexes.nameidx, {
+                        unique: false,
+                        fields: ['name']
+                    });
+
+                    cb(); 
+                });
+            },
+            function (cb) {
+                var schema = function () {
+                    this.table("people", function () {
+                        this.field('id', 'integer', { primary: true });
+                        this.field('name', 'text');
+                            
+                        this.index('nameidx', ['id', 'name'], true);
+                    });
+                };
+
+                testSchema(db, schema, function (err, schema) {
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    assert.deepEqual(schema.tables.people.indexes.nameidx, {
+                        unique: true,
+                        fields: ['id', 'name']
+                    });
+
+                    cb(); 
+                });
+            }
+        ], done);
     });
 });
